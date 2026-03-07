838e0fcc5a7722b7803092794d2dee411fafcfec
Aashish Chauhan <aashishchauhan2172@gmail.com>
2026-03-07 14:35:02 +0530

    Updated

--- src/components/Versatile.jsx
+++ src/components/Versatile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import v1 from "../assets/Home/versatile1.svg";
 import v2 from "../assets/Home/versatile2.png";
 import v3 from "../assets/Home/versatile3.png";
@@ -8,21 +9,24 @@ import v6 from "../assets/Home/versatile6.png";
 import v7 from "../assets/Home/versatile7.png";
 import v8 from "../assets/Home/versatile8.png";
 import v9 from "../assets/Home/versatile9.png";
+
 const versatileImages = [v1, v2, v3, v4, v5, v6, v7, v8, v9];
-const Versatile = ({ index }) => {
+
+const Versatile = ({ index = 0 }) => {
   return (
-    <section className="relative w-full h-[60vh] sm:h-[70vh] md:h-[80vh] lg:h-screen overflow-hidden bg-black">
-      {" "}
+    <section className="relative w-full min-h-[60vh] sm:min-h-[70vh] md:min-h-[80vh] lg:min-h-screen overflow-hidden bg-black">
       <img
         src={versatileImages[index]}
         alt="Versatile Section"
-        className="w-full h-full object-cover"
-      />{" "}
+        className="w-full h-full object-cover object-center"
+      />
+
       <button
         onClick={() => console.log("Contact Clicked")}
-        className="absolute bottom-[10%] sm:bottom-[15%] left-[5%] sm:left-[10%] w-[120px] sm:w-[150px] h-[40px] sm:h-[50px] cursor-pointer bg-transparent"
-      />{" "}
+        className="absolute bottom-[8%] sm:bottom-[12%] md:bottom-[14%] lg:bottom-[15%] left-[6%] sm:left-[8%] md:left-[10%] w-[110px] sm:w-[140px] md:w-[160px] h-[38px] sm:h-[45px] md:h-[50px] bg-transparent cursor-pointer"
+      />
     </section>
   );
 };
+
 export default Versatile;
