0566fd4f9cc9fc02e17bc33612c14930691e7ace
Aashish Chauhan <aashishchauhan2172@gmail.com>
2026-03-07 14:42:50 +0530

    Updated

--- src/components/Versatile.jsx
+++ src/components/Versatile.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import v1 from "../assets/Home/versatile1.svg";
 import v2 from "../assets/Home/versatile2.png";
 import v3 from "../assets/Home/versatile3.png";
@@ -14,16 +13,16 @@ const versatileImages = [v1, v2, v3, v4, v5, v6, v7, v8, v9];
 
 const Versatile = ({ index = 0 }) => {
   return (
-    <section className="relative w-full min-h-[60vh] sm:min-h-[70vh] md:min-h-[80vh] lg:min-h-screen overflow-hidden bg-black">
+    <section className="relative w-full overflow-hidden bg-black">
       <img
         src={versatileImages[index]}
         alt="Versatile Section"
-        className="w-full h-full object-cover object-center"
+        className="w-full h-auto object-contain"
       />
 
       <button
         onClick={() => console.log("Contact Clicked")}
-        className="absolute bottom-[8%] sm:bottom-[12%] md:bottom-[14%] lg:bottom-[15%] left-[6%] sm:left-[8%] md:left-[10%] w-[110px] sm:w-[140px] md:w-[160px] h-[38px] sm:h-[45px] md:h-[50px] bg-transparent cursor-pointer"
+        className="absolute bottom-[12%] left-[8%] w-[120px] h-[40px] bg-transparent cursor-pointer"
       />
     </section>
   );
